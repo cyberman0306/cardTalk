2a231b63c1aff812036102ac1b5faa3cb550ecaa
yook <yhj03069@gmail.com>
2024-02-04 12:02:21 +0900

    Initial Commit

---
+++ cardTalk/cardTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardTalkApp.swift
+//  cardTalk
+//
+//  Created by yook on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct cardTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
